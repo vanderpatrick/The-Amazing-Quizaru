1e49f35b5ec99786a739aca2d086e9f9107f3bbd
vanderpatrick <patrick123321123@icloud.com>
2022-01-28 16:31:19 +0000

    Add questions script file to all html pages and add callGame function

---
+++ assets/js/questions.js
@@ -0,0 +1,56 @@
+let questions = [{
+        numb: 1,
+        question: "Who was the first skater to do a 900 ? ",
+        answer: "Tony Hawk",
+        options: [
+            "Tony Hawk",
+            "Danny Way",
+            "Eric Koston",
+            "Chris Joslin"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Where was Skateboarding invented ?",
+        answer: "California USA",
+        options: [
+            "Brazil",
+            "California USA",
+            "Japan",
+            "Netherlands"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the options bellow is a hidden skate rule",
+        answer: "Wait your goddam tur",
+        options: [
+            "Never eat in the park",
+            "Dont smoke in the park",
+            "Dont skate mongo",
+            "Wait your goddam turn"
+        ]
+
+    },
+    {
+        numb: 4,
+        question: "What is Tony Hawk Skate style ?",
+        answer: "Vert",
+        options: [
+            "street",
+            "Free Style",
+            "Vert",
+            "Hill Bomb"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Who ollied across the Great Wall Of China ?",
+        answer: "Danny Way",
+        options: [
+            "Rodney Mullen",
+            "Paul Rodriguez",
+            "Sean Malto",
+            "Danny Way"
+        ]
+    }]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
+// Function to call The burger menu.
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle('active')
 })
+
+// Funtion  to call questions and options in the quiz.
+callGame()
+function callGame() {
+    const quest_text = document.querySelector('.question');
+    const option_text = document.querySelector('.options');
+
+    let quest_content = '<span>' + questions[0].numb + '.' + questions[0].question + '</span>';
+    let option_content = '<div class="option">' + '<span>' + questions[0].options[0] + '</span>' + '</div>' 
+    + '<div class="option">' + '<span>' + questions[0].options[1] + '</span>' + '</div>'
+    + '<div class="option">' + '<span>' + questions[0].options[2] + '</span>' + '</div>'
+    + '<div class="option">' + '<span>' + questions[0].options[3] + '</span>' + '</div>'
+    option_text.innerHTML = option_content
+    quest_text.innerHTML = quest_content
+}
